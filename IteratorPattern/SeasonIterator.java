7e0179fc5ee11dd14d5d6bc284658e3c6dbb1cd7
aiggerim <162737141+aiggerim@users.noreply.github.com>
2025-04-25 16:27:32 +0500

    Fix SeasonIterator: add imports and remove() override

--- IteratorPattern/SeasonIterator.java
+++ IteratorPattern/SeasonIterator.java
@@ -1,4 +1,28 @@
 package IteratorPattern;
 
-public class SeasonIterator {
+import java.util.Iterator;
+import java.util.List;
+
+public class SeasonIterator implements EpisodeIterator, Iterator<Episode> {
+    private final List<Episode> episodes;
+    private int index = 0;
+
+    public SeasonIterator(Season season) {
+        this.episodes = season.getEpisodes();
+    }
+
+    @Override
+    public boolean hasNext(){
+        return index < episodes.size();
+    }
+
+    @Override
+    public Episode next() {
+        return episodes.get(index++);
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException("Remove not supported.");
+    }
 }
