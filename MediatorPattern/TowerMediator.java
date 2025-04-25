fa127095c35c1b8ce6cac0fcf5abc91001f59a75
aiggerim <162737141+aiggerim@users.noreply.github.com>
2025-04-25 18:37:13 +0500

    Add TowerMediator interface for coordinating aircraft

---
+++ MediatorPattern/TowerMediator.java
@@ -0,0 +1,9 @@
+package MediatorPattern;
+
+import MediatorPattern.aircraft.Aircraft;
+
+public interface TowerMediator {
+    void broadcast(String msg, Aircraft sender);
+    boolean requestRunway(Aircraft a); // true if granted
+}
+
