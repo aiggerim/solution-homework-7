146f127319e5dae7968d248f4b0b091cc8748215
aiggerim <162737141+aiggerim@users.noreply.github.com>
2025-04-25 16:31:44 +0500

    Add ReverseSeasonIterator for reverse episode traversal

--- IteratorPattern/ReverseSeasonIterator.java
+++ IteratorPattern/ReverseSeasonIterator.java
@@ -1,2 +1,23 @@
-package IteratorPattern;public class ReverseSeasonIterator {
+package IteratorPattern;
+
+import java.util.List;
+
+public class ReverseSeasonIterator implements EpisodeIterator {
+    private final List<Episode> episodes;
+    private int index;
+
+    public ReverseSeasonIterator(Season season){
+        this.episodes = season.getEpisodes();
+        this.index = episodes.size() - 1;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index >= 0;
+    }
+
+    @Override
+    public Episode next(){
+        return episodes.get(index--);
+    }
 }
